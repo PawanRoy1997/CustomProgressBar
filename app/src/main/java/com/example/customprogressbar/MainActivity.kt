8b81d2557ffeb5c124a3ce2b3bbcf933389b3565
PawanRoy <pawanroy0066@gmail.com>
2022-05-16 17:41:31 +0530

    Implemented

--- app/src/main/java/com/example/customprogressbar/MainActivity.kt
+++ app/src/main/java/com/example/customprogressbar/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.customprogressbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customprogressbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var isStopped = true
+    var goalProgress = 0
+    var expectedGoal = 50
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.goalProgressBar.apply {
+            setProgress(100, false)
+            setGoal(50)
+        }
+
+        binding.restartBtn.setOnClickListener {
+            goalProgress = 0
+            if (isStopped) {
+                isStopped = false
+                expectedGoal = (0..100).random()
+                binding.goalProgressBar.setGoal(expectedGoal)
+                while (goalProgress < 100) {
+                    goalProgress++
+                    binding.goalProgressBar.setProgress(goalProgress)
+                }
+                isStopped = true
+            }
+        }
     }
 }
